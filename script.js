935dd5496b2f84f322b9291705a3e21223a96e52
GENESSISJ\Usuario <genesis.jz.93@gmail.com>
2018-02-07 10:59:56 -0400

    Casos base listos

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-var text = "";
+var code = "";
 
 function random() {
     var possible = "RAMVNI";
 
     for (var i = 0; i < 4; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+        code += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return text;
+    code = "NRRI";
+    return code;
 }
 
 console.log(random());
 
 function verifyCode() {
-    var code = document.getElementById("code").value;
+    var inputCode = document.getElementById("inputCode").value;
+    var equis = "";
+    var asterisk = "";
+
     console.log(code);
-    console.log(text);
+    console.log(inputCode);
+
+    for (var i = 0; i < inputCode.length; i++) {
+        var e = inputCode.charAt(i);
+        for (var j = 0; j < code.length; j++) {
+            if (e == code.charAt(j) && i==j ) {
+                console.log("i: ", i);
+                console.log("j: ", j);
+                equis += "X";
+            } else if (e == code.charAt(j)) {
+                console.log("i: ", i);
+                console.log("j: ", j);
+                asterisk += "*";
+            }
+        }
+    }
+    console.log(equis+asterisk);
 }
